e2fecd0ceea85ce53d69cbe7c80517cf8ca2275a
Huxley Dowling <hdowling87@hotmail.com>
2024-01-15 21:40:07 +1000

    Updates, still can't connect to DB

--- BlogApi2/BlogApi2/Repositories/BlogRepository.cs
+++ BlogApi2/BlogApi2/Repositories/BlogRepository.cs
@@ -22,48 +22,33 @@ namespace BlogApi2.Repositories
         {
             FilterDefinition<Blog> filter = Builders<Blog>.Filter.Eq(p => p.Id, id);
 
-            DeleteResult deleteResult = await _blogContext
-                                                .Blogs
-                                                .DeleteOneAsync(filter);
+            DeleteResult deleteResult = await _blogContext.Blogs .DeleteOneAsync(filter);
 
-            return deleteResult.IsAcknowledged
-                && deleteResult.DeletedCount > 0;
+            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
         }
 
         public async Task<Blog> GetBlog(string id)
         {
-            return await _blogContext
-                           .Blogs
-                           .Find(p => p.Id == id)
-                           .FirstOrDefaultAsync();
+            return await _blogContext.Blogs.Find(p => p.Id == id).FirstOrDefaultAsync();
         }
 
         public async Task<IEnumerable<Blog>> GetBlogByTitle(string name)
         {
             FilterDefinition<Blog> filter = Builders<Blog>.Filter.ElemMatch(p => p.Title, name);
 
-            return await _blogContext
-                            .Blogs
-                            .Find(filter)
-                            .ToListAsync();
+            return await _blogContext.Blogs.Find(filter).ToListAsync();
         }
 
         public async Task<IEnumerable<Blog>> GetBlogs()
         {
-            return await _blogContext
-                            .Blogs
-                            .Find(p => true)
-                            .ToListAsync();
+            return await _blogContext.Blogs.Find(p => true).ToListAsync();
         }
 
         public async Task<bool> UpdateBlog(Blog blog)
         {
-            var updateResult = await _blogContext
-                            .Blogs
-                            .ReplaceOneAsync(filter: g => g.Id == blog.Id, replacement: blog);
+            var updateResult = await _blogContext.Blogs.ReplaceOneAsync(filter: g => g.Id == blog.Id, replacement: blog);
 
-            return updateResult.IsAcknowledged
-                    && updateResult.ModifiedCount > 0;
+            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
         }
     }
 }
